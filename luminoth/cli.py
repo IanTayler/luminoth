5dba520743081447b8e25d43ec23e735185f8408
Javier Rey <javirey@gmail.com>
2017-08-08 11:19:31 -0300

    Adds basic eval script (no metrics yet)

--- luminoth/cli.py
+++ luminoth/cli.py
@@ -2,6 +2,7 @@ import click
 
 from .tools import cloud, dataset
 from .train import train
+from .eval import evaluate
 
 
 @click.group()
@@ -12,3 +13,4 @@ def cli():
 cli.add_command(cloud)
 cli.add_command(dataset)
 cli.add_command(train)
+cli.add_command(evaluate)
